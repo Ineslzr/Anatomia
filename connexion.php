a454409077f645ac01664f7bc4e6b1aef8ee0a84
darin <darin@DESKTOP-TDUGOO1>
2020-12-25 11:09:19 +0100

    page d'accueil canvas

---
+++ connexion.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <link href="style.css" rel="stylesheet">
+
+	<title>Accueil</title>
+</head>
+	<body>
+		<header>
+			<?php
+				require_once('menu.php');
+			?>
+		</header>
+		<footer>
+			
+		</footer>
+   		<h1>Connexion</h1>
+	</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,53 @@
+
+<div class="banner">
+	<div class="baniere-top">
+		<img src="image-accueil/baniere.png" class="img-fluid" alt="Responsive image">
+	</div>
+</div>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<div class="container-fluid">	    	
+		<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+		    <ul class="navbar-nav">
+		        <li class="nav-item">
+	    			<a class="nav-link active" aria-current="page" href="/Home">
+					<img src="image-accueil/home.png" width="30" height="30" alt=""></a>
+	    		</li>
+	    		<li class="nav-item">
+	    			<a class="nav-link" href="#">Anatomie</a>
+	    		</li>
+	    		<li class="nav-item">
+	    			<a class="nav-link" href="#">Ensegnements</a>
+	    		</li>
+	    		<li class="nav-item">
+	    			<a class="nav-link" href="#">Quizz</a>
+	    		</li>
+	    		<li class="nav-item">
+	    			<a class="nav-link" href="#">Dictionnaire</a>
+	    		</li>
+	    		<li class="nav-item">
+	    			<a class="nav-link" href="#">Discussion</a>
+	    		</li>
+	    		<li class="nav-item">
+	    			<a class="nav-link" href="#">Contact</a>
+	    		</li>
+		    </ul>
+  			<div class="container-fluid">
+   				<form class="d-flex">
+      				<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      				<button class="btn btn-outline-success" type="submit">Search</button>
+    			</form>
+  			</div>
+  			<div>
+		      	<ul class="navbar-nav ml-md-auto" >
+			        <li class="nav-item">
+			        	<a class="nav-link" href="inscription.php">S'inscrire</a>
+			        </li>
+			        <li>
+			        	<a class="nav-link" href="connexion.php"><NOBR>Se connecter</a>
+			        </li>
+			    </ul>
+			</div>
+	   </div>
+  	</div>
+</nav>
